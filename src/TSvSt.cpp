0156c1ae8a02dde0415147e7d99cee795e73f1aa
pawel <plapacz6@gmail.com>
2024-06-30 09:43:29 +0200

    TSvSt

--- src/TSvSt.cpp
+++ src/TSvSt.cpp
@@ -1,3 +1,21 @@
+/*
+Copyright 2024-2024 plapacz6@gmail.com
+
+This file is part of net_comm_dbg.
+
+net_comm_dbg is free software: you can redistribute it and/or modify it under the terms
+of the GNU Lesser General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+net_comm_dbg is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License along
+ with net_comm_dbg. If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include "TSvSt.h"
 #include <cstdio>
 #include <iostream>
